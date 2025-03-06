804ed6e000c6e69383504bada181025da29f8a90
aswanthup06 <aswanthup06@gmail.com>
2025-03-06 15:41:18 +0530

    aswanth

--- src/app/crolist/page.js
+++ src/app/crolist/page.js
@@ -3,7 +3,7 @@ import Layout from "../../components/layout";
 const crolist = () => {
     return (
       <Layout>
-        <h1 className="text-3xl font-bold mb-4">Page 1 dhsjkdhsjkdhjkss</h1>
+        <h1 className="text-3xl font-bold mb-4">Aswanth Up</h1>
         <p>This is Page 1.</p>
       </Layout>
     );
