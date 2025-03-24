ed496c28fff599de4ecfa939b9565d9fee475434
aswanthup06 <aswanthup06@gmail.com>
2025-03-24 19:25:22 +0530

    Export-Invoice

--- src/app/cro-list/export-invoice/page.js
+++ src/app/cro-list/export-invoice/page.js
@@ -6,12 +6,14 @@ import Layout from "../../../components/layout";
 import Progress from "../../cro-list/components/progress";
 import ProgressButton from "../../cro-list/components/progress-buttons";
 import { GoPlus } from "react-icons/go";
+import { MdOutlineFileDownload } from "react-icons/md";
 
 
 
 
 const exportinvoice = () => {
   const data = [
+    { fechExchangeRate: "Currency", addExchangeRate: "84.5" },
     { fechExchangeRate: "Exchange Rate", addExchangeRate: "84.5" },
     { fechExchangeRate: "Date", addExchangeRate: "84.5" },
   ];
@@ -56,6 +58,16 @@ const exportinvoice = () => {
         >
           Final Invoice
         </button>
+        <button
+          className={`px-6 h-8 text-xs ml-3 rounded-es-lg rounded-se-lg transition-all duration-200 cursor-pointer w-40 ${
+            activePage === 'final'
+              ? 'bg-blue-500 text-white hover:bg-blue-600'
+              : 'bg-gray-200 text-gray-800 hover:bg-blue-200'
+          }`}
+          onClick={() => setActivePage('final')}
+        >
+          Additional Invoice
+        </button>
       </div>
 
       {activePage === 'proforma' && (
@@ -66,35 +78,41 @@ const exportinvoice = () => {
 
           <div className="grid grid-cols-2 mt-4 gap-12">
             <div>
-              <InputField label="CRO Number" id="croNumber" placeholder="Lorem" />
-              <InputField label="Validity" id="validity" placeholder="Lorem" />
+              <InputField label="Bill of Lading" id="croNumber" placeholder="Lorem" />
+              <InputField label="Dated" id="validity" placeholder="Lorem" />
             </div>
 
             <div>
-              <InputField label="Validity" id="validity2" placeholder="Lorem" />
-              <InputField label="Cargo Type" id="cargoType" placeholder="Lorem" />
+              <InputField label="Load Port" id="validity2" placeholder="Lorem" />
+              <InputField label="Discharge Port" id="cargoType" placeholder="Lorem" />
             </div>
           </div>
 
           <div className="relative p-[1px] rounded-lg mt-6 bg-gradient-to-r from-pink-400 to-blue-400">
-            <div className="bg-white rounded-lg pb-6 pt-3">
-              <table className="w-full border-collapse">
-                <thead>
-                  <tr>
-                    <th className="p-3 border-b border-gray-200 text-center text-xs font-semibold">Fech Exchange Rate</th>
-                    <th className="p-3 border-b border-gray-200 text-center text-xs font-semibold">Add Exchange Rate</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {data.map((row, index) => (
-                    <tr key={index} className="hover:bg-gray-50">
-                      <td className="p-3 border-b border-gray-200 text-center text-xs">{row.fechExchangeRate}</td>
-                      <td className="p-3 border-b border-gray-200 text-center text-xs">{row.addExchangeRate}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
+          <div className="bg-white rounded-lg p-6 pt-3">
+  <table className="w-full border-collapse">
+    <thead>
+      <tr>
+        <th className="w-1/2 p-3 border-b border-gray-200 text-left text-xs font-semibold">Fech Exchange Rate</th>
+        <th className="w-1/2 p-3 border-b border-gray-200 text-center text-xs font-semibold">Add Exchange Rate</th>
+      </tr>
+    </thead>
+    <tbody>
+      {data.map((row, index) => (
+        <tr key={index} className="hover:bg-gray-50">
+          <td className="w-1/2 p-3 border-b border-gray-200 text-left text-xs">{row.fechExchangeRate}</td>
+          <td className="w-1/2 px-4 text-sm text-gray-700 border-b border-gray-200 text-center">
+            <input
+              type="text"
+              className="w-full px-2 py-2 border border-white focus:outline-none focus:ring-2 focus:ring-blue-500 text-center"
+              placeholder="Type here"
+            />
+          </td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+</div>
           </div>
 
           <div className="flex justify-end mt-4">
@@ -105,53 +123,79 @@ const exportinvoice = () => {
 
           <div className="relative p-[1px] rounded-lg mt-6 bg-gradient-to-r from-pink-400 to-blue-400">
   <div className="rounded-md bg-white p-2  pb-4">
-    <div className="">
-      <table className="min-w-full bg-white">
-        <thead>
-          <tr>
-            <th className="px-4 py-2 text-sm font-semibold text-gray-700 border-b border-gray-200">SrNo</th>
-            <th className="px-4 py-2 text-sm font-semibold text-gray-700 border-b border-gray-200">Charge Head</th>
-            <th className="px-4 py-2 text-sm font-semibold text-gray-700 border-b border-gray-200">Quantity</th>
-            <th className="px-4 py-2 text-sm font-semibold text-gray-700 border-b border-gray-200">Rate</th>
-            <th className="px-4 py-2 text-sm font-semibold text-gray-700 border-b border-gray-200">Amount</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tableData.map((item, index) => (
-            <tr key={item.id} className="hover:bg-gray-50">
-              <td className="px-4 text-sm text-gray-700 border-b border-gray-200 text-center">{index + 1}</td>
-              <td className="px-4 text-sm text-gray-700 border-b border-gray-200 text-center">
-                <input
-                  type="text"
-                  className="w-full px-2 py-2 border border-white focus:outline-none focus:ring-2 focus:ring-blue-500 text-center"
-                  defaultValue={item.chargeHead}
-                />
-              </td>
-              <td className="px-4 text-sm text-gray-700 border-b border-gray-200 text-center">
-                <input
-                  type="number"
-                  className="w-full px-2 py-2 border border-white focus:outline-none focus:ring-2 focus:ring-blue-500 text-center"
-                  defaultValue={item.quantity}
-                />
-              </td>
-              <td className="px-4 text-sm text-gray-700 border-b border-gray-200 text-center">{item.rate}</td>
-              <td className="px-4 text-sm text-gray-700 border-b border-gray-200 text-center">{item.amount}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+  <div className="">
+  <table className="min-w-full bg-white">
+    <thead>
+      <tr>
+        <th className="px-4 py-2 text-sm font-semibold text-gray-700 border-b border-gray-200">SrNo</th>
+        <th className="px-4 py-2 text-sm font-semibold text-gray-700 border-b border-gray-200">Charge Head</th>
+        <th className="px-4 py-2 text-sm font-semibold text-gray-700 border-b border-gray-200">Quantity</th>
+        <th className="px-4 py-2 text-sm font-semibold text-gray-700 border-b border-gray-200">Rate</th>
+        <th className="px-4 py-2 text-sm font-semibold text-gray-700 border-b border-gray-200">Amount</th>
+        <th className="px-4 py-2 text-sm font-semibold text-gray-700 border-b border-gray-200">Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      {tableData.map((item, index) => (
+        <tr key={item.id} className="hover:bg-gray-50">
+          <td className="px-4 text-sm text-gray-700 border-b border-gray-200 text-center">{index + 1}</td>
+          <td className="px-4 text-sm text-gray-700 border-b border-gray-200 text-center">
+            <input
+              type="text"
+              className="w-full px-2 py-2 border border-white focus:outline-none focus:ring-2 focus:ring-blue-500 text-center"
+              placeholder="Type here"
+            />
+          </td>
+          <td className="px-4 text-sm text-gray-700 border-b border-gray-200 text-center">
+            <input
+              type="number"
+              className="w-full px-2 py-2 border border-white focus:outline-none focus:ring-2 focus:ring-blue-500 text-center"
+              placeholder="Type here"
+            />
+          </td>
+          <td className="px-4 text-sm text-gray-700 border-b border-gray-200 text-center">
+            <input
+              type="number"
+              className="w-full px-2 py-2 border border-white focus:outline-none focus:ring-2 focus:ring-blue-500 text-center"
+              placeholder="Type here"
+            />
+          </td>
+          <td className="px-4 text-sm text-gray-700 border-b border-gray-200 text-center">
+            <input
+              type="number"
+              className="w-full px-2 py-2 border border-white focus:outline-none focus:ring-2 focus:ring-blue-500 text-center"
+              placeholder="Type here"
+            />
+          </td>
+          <td className="px-4 text-sm text-gray-700 border-b border-gray-200 text-center">
+            <button className='bg-blue-500 text-xs rounded-md py-2 px-4 text-white'>Update</button>
+          </td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+</div>
   </div>
 </div>
 
 
-          <div className="flex justify-end mt-4">
+          <div className="flex justify-between mt-4">
+
+            <div className='flex gap-2'>
+
+              <button className='text-xs px-6 h-8 rounded-md text-white bg-blue-500 hover:bg-blue-600  transition-all duration-200 cursor-pointer flex items-center justify-center gap-2'>Download Freight <MdOutlineFileDownload /></button>
+              <button className='text-xs px-6 h-8 rounded-md text-white bg-amber-600 hover:bg-amber-700  transition-all duration-200 cursor-pointer flex items-center justify-center gap-2'>Download THC<MdOutlineFileDownload /></button>
+
+            </div>
+
+<div>
             <button className="bg-blue-500 text-white px-6 h-8 text-xs ml-3 rounded-es-lg rounded-se-lg hover:bg-blue-600 transition-all duration-200 cursor-pointer w-24">
               Save
             </button>
             <button className="bg-white border-2 border-gray-300 text-gray-900 hover:bg-gray-100 hover:border-gray-400 px-6 h-8 text-xs ml-3 rounded-es-lg rounded-se-lg  transition-all duration-200 cursor-pointer w-24">
               Submit
             </button>
+</div>
           </div>
         </div>
       )}
