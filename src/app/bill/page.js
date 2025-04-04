43082494a75ca9c2b28280c1b71ad3f58fb6b8f7
aswanthup06 <aswanthup06@gmail.com>
2025-04-04 13:26:11 +0530

    bill

---
+++ src/app/bill/page.js
@@ -0,0 +1,201 @@
+import React from "react";
+
+function page() {
+  return (
+    <div className="bg-gray-100 ">
+      <div className="w-[595px] min-h-[842px] max-h-[842px] bg-white mx-auto text-green-900 flex flex-col justify-between">
+
+<div>
+        <div className="h-6 bg-green-700"></div>
+        <div className="px-6 pt-6">
+          <div className="border-2 border-green-900 rounded-md">
+            <h1 className="text-center font-bold py-3">FIRST ORIGINAL</h1>
+
+            <div className="grid grid-cols-2">
+              <div>
+                <table className="h-full w-full">
+                  <tr>
+                    <td className=" border-y-2 border-r-2 border-green-900 p-2 align-top">
+                      <h1 className="text-xs font-bold">MUNDRA INDIA</h1>
+                      <h1 className="text-xs">Place of receipt</h1>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className=" border-y-2 border-r-2 border-green-900 p-2 align-top">
+                      <h1 className="text-xs font-bold">MUNDRA INDIA</h1>
+                      <h1 className="text-xs">Place of receipt</h1>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className=" border-y-2 border-r-2 border-green-900 p-2 align-top">
+                      <h1 className="text-xs font-bold">MUNDRA INDIA</h1>
+                      <h1 className="text-xs">Place of receipt</h1>
+                    </td>
+                  </tr>
+                </table>
+              </div>
+              <div>
+                <table className="w-full table-fixed">
+                  <tr>
+                    <td className="w-1/2 border-y-2 border-green-900 p-2 align-top">
+                      <h1 className="text-xs font-bold">MUNDRA INDIA</h1>
+                      <h1 className="text-xs">Place of receipt </h1>
+                    </td>
+                    <td className="w-1/2 border-y-2 border-l-2 border-green-900 p-2 align-top">
+                      <h1 className="text-xs font-bold">MUNDRA INDIA</h1>
+                      <h1 className="text-xs">Place of receipt</h1>
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <td className="w-1/2 border-y-2 border-green-900 p-2 align-top">
+                      <h1 className="text-xs font-bold">MUNDRA INDIA</h1>
+                      <h1 className="text-xs">Place of receipt</h1>
+                    </td>
+                    <td className="w-1/2 border-y-2 border-l-2 border-green-900 p-2 align-top">
+                      <h1 className="text-xs font-bold">MUNDRA INDIA</h1>
+                      <h1 className="text-xs">Place of receipt</h1>
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <td className="p-2 border-b-2 border-green-900" colSpan="2">
+                      <div className="flex justify-center">
+                        <img
+                          src="https://raw.githubusercontent.com/musthafa-pk/publicassets/refs/heads/main/logo.jpg"
+                          alt="Logo"
+                          className="w-48"
+                        />
+                      </div>
+                    </td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+
+            <div className="w-full">
+              <table className="w-full table-fixed">
+                <tr>
+                  <td className="w-1/4 border-b-2 border-green-900 p-2">
+                    <h1 className="text-xs">Place of receipt</h1>
+                    <h1 className="text-xs font-bold">MUNDRA INDIA</h1>
+                  </td>
+                  <td className="w-1/4 border-b-2 border-l-2  border-green-900 p-2">
+                    <h1 className="text-xs">Place of receipt</h1>
+                    <h1 className="text-xs font-bold">MUNDRA INDIA</h1>
+                  </td>
+                  <td className="w-1/4 border-b-2 border-green-900 p-2">
+                    <h1 className="text-xs">Place of receipt</h1>
+                    <h1 className="text-xs font-bold">MUNDRA INDIA</h1>
+                  </td>
+                  <td className="w-1/4 border-b-2 border-l-2 border-green-900 p-2">
+                    <h1 className="text-xs">Place of receipt</h1>
+                    <h1 className="text-xs font-bold">MUNDRA INDIA</h1>
+                  </td>
+                </tr>
+                <tr>
+                  <td className="w-1/4 border-b-2 border-green-900 p-2">
+                    <h1 className="text-xs">Place of receipt</h1>
+                    <h1 className="text-xs font-bold">MUNDRA INDIA</h1>
+                  </td>
+                  <td className="w-1/4 border-b-2 border-l-2 border-green-900 p-2">
+                    <h1 className="text-xs">Place of receipt</h1>
+                    <h1 className="text-xs font-bold">MUNDRA INDIA</h1>
+                  </td>
+                  <td className="w-1/4 border-b-2 border-green-900 p-2">
+                    <h1 className="text-xs">Place of receipt</h1>
+                    <h1 className="text-xs font-bold">MUNDRA INDIA</h1>
+                  </td>
+                  <td className="w-1/4  border-l-2 border-b-2 border-green-900 p-2">
+                    <h1 className="text-xs">Place of receipt</h1>
+                    <h1 className="text-xs font-bold">MUNDRA INDIA</h1>
+                  </td>
+                </tr>
+              </table>
+            </div>
+
+            <div>
+              <table className="w-full border-collapse">
+                <thead>
+                  <tr>
+                    <th className="w-[30%] border-b-2 border-green-900 p-2 text-left text-xs font-bold">
+                      Marks & Number
+                    </th>
+                    <th className="w-[18%] border-b-2 border-l-2 border-green-900  p-2 text-left text-xs font-bold">
+                      No. of Pkg.
+                    </th>
+                    <th className="w-[40%] border-b-2 border-l-2 border-green-900  p-2 text-left text-xs font-bold">
+                      Description of Goods & Pkg
+                    </th>
+                    <th className="w-[18%] border-b-2 border-l-2 border-green-900  p-2 text-left text-xs font-bold">
+                      Gross Weight
+                    </th>
+                    <th className="w-[30%] border-b-2 border-l-2 border-green-900 p-2 text-left text-xs font-bold">
+                      Measurements
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className="h-36">
+                    <td className="p-2 text-xs align-top">ABC123</td>
+                    <td className="border-l-2 border-green-900  p-2 text-xs align-top">
+                      5
+                    </td>
+                    <td className="border-l-2 border-green-900  p-2 text-xs align-top">
+                      Electronics components in carton
+                    </td>
+                    <td className="border-l-2 border-green-900  p-2 text-xs align-top">
+                      25.5 kg
+                    </td>
+                    <td className="border-l-2 border-green-900  p-2 text-xs align-top">
+                      40x30x20 cm
+                    </td>
+                  </tr>
+                  <tr className="">
+                    <td className="border-b-2 border-green-900 p-2 text-xs align-top"></td>
+                    <td className="border-2 border-green-900 p-2 text-xs align-top">
+                      Total
+                    </td>
+                    <td className="border-b-2 border-green-900 p-2 text-xs align-top"></td>
+                    <td className="border-b-2 border-l-2 border-green-900 p-2 text-xs align-top"></td>
+                    <td className="border-b-2 border-l-2 border-green-900 p-2 text-xs align-top"></td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+
+            <div className="w-full">
+              <table className="w-full table-fixed">
+                <tr>
+                  <td className="w-4/12 p-2 align-top">
+                    <h1 className="text-xs">Place of receipt</h1>
+                    <h1 className="text-xs font-bold">MUNDRA INDIA</h1>
+                  </td>
+                  <td className="w-8/12 border-l-2 border-green-900 p-2 align-top">
+                    <h1 className="text-xs">
+                      Morning is the beginning of a new day, filled with fresh
+                      energy and endless possibilities. As the sun rises and the
+                      world slowly awakens, the air feels crisp and calm,
+                      offering a sense of peace and renewal. It is a time when
+                      nature is at its most beautiful, with soft golden light
+                      and the gentle sounds of birds chirping. 
+                    </h1>
+                    <h1 className="text-xs font-bold">MUNDRA INDIA</h1>
+                  </td>
+                </tr>
+              </table>
+            </div>
+          </div>
+        </div>
+</div>
+
+<div>
+    <img src="/footer.png" alt="" />
+</div>
+
+      </div>
+    </div>
+  );
+}
+
+export default page;
